0c929efd12f8164f1b89ab3feaea28511d30d944
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-08-26 08:59:53 +0300

    One final deployment issue

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 rm -rf jseminck-be-api-expenses
 mkdir jseminck-be-api-expenses
-mv ./jseminck-be-api-expenses.tgz jseminck-be-api-authentication/jseminck-be-api-expenses.tgz
+mv ./jseminck-be-api-expenses.tgz jseminck-be-api-expenses/jseminck-be-api-expenses.tgz
 cd jseminck-be-api-expenses
 tar -zxvf jseminck-be-api-expenses.tgz
 node-gyp rebuild
