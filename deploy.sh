f17f47c695803068bdc2c6e65e02534b2b18d30a
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-08-26 08:54:47 +0300

    Really fix travis

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-rm -rf jseminck-be-api-authentication
-mkdir jseminck-be-api-authentication
-mv ./jseminck-be-api-authentication.tgz jseminck-be-api-authentication/jseminck-be-api-authentication.tgz
-cd jseminck-be-api-authentication
-tar -zxvf jseminck-be-api-authentication.tgz
+rm -rf jseminck-be-api-expenses
+mkdir jseminck-be-api-expenses
+mv ./jseminck-be-api-expenses.tgz jseminck-be-api-authentication/jseminck-be-api-expenses.tgz
+cd jseminck-be-api-expenses
+tar -zxvf jseminck-be-api-expenses.tgz
 node-gyp rebuild
 npm install
 npm run stop
 npm run start
-rm jseminck-be-api-authentication.tgz
+rm jseminck-be-api-expenses.tgz
 cd ..
 rm deploy.sh
